c5e4a3a9667f3a6616d1658de6b8b33b14aad0c1
Tommaso Clini <tommaso@localhost.localdomain>
2024-06-14 09:18:07 +0200

    not working yet

--- remote_ctrl/remote_ctrl.c
+++ remote_ctrl/remote_ctrl.c
@@ -10,6 +10,10 @@ static void button_double_click_cb(void *button_handle, void *usr_data)
 {
     ESP_LOGI(TAG, "BUTTON_DOUBLE_CLICK");
 
+    remote_ctrl_bind_config_t config;
+    memcpy(&config, &usr_data, sizeof(config));
+    ESP_LOGI(TAG, "Config{init_attr : %" PRIi16 ", rssi : %" PRIi8 ", wait_ms : %" PRIu16 ", resp_to_init_ms : %" PRIu16 "}, size of config: %u", config.init_attr, config.rssi, config.wait_ms, config.resp_to_init_ms, sizeof(config));
+
     ESP_LOGI(TAG, "Doing bind operation");
 }
 
@@ -24,7 +28,7 @@ static void button_multiple_click_cb(void *button_handle, void *usr_data)
 {
     ESP_LOGI(TAG, "BUTTON_MULTIPLE_CLICK");
 
-    espnow_ctrl_responder_clear_bindlist();
+    //espnow_ctrl_responder_clear_bindlist();
     ESP_LOGI(TAG, "Cleared remote control(espnow) bindlist");
 }
 
@@ -32,11 +36,11 @@ static void button_single_click_cb(void *button_handle, void *usr_data)
 {
     ESP_LOGI(TAG, "BUTTON_SINGLE_CLICK");
 
-    espnow_ctrl_initiator_send(ESPNOW_ATTRIBUTE_BASE, ESPNOW_ATTRIBUTE_BASE, esp_log_timestamp());
+    //espnow_ctrl_initiator_send(ESPNOW_ATTRIBUTE_BASE, ESPNOW_ATTRIBUTE_BASE, esp_log_timestamp());
     ESP_LOGI(TAG, "Sending test message");
 }
 
-esp_err_t init_for_all()
+esp_err_t remote_ctrl_init_for_all()
 {
     if (all_init)
         return ESP_ERR_INVALID_STATE;
@@ -48,18 +52,23 @@ esp_err_t init_for_all()
     espnow_config_t espnow_config = ESPNOW_INIT_CONFIG_DEFAULT();
     ESP_RETURN_ON_ERROR(espnow_init(&espnow_config), TAG, "Failed to init espnow");
 
+    all_init = true;
+
     return ESP_OK;
 }
-esp_err_t deinit_for_all()
+esp_err_t remote_ctrl_deinit_for_all()
 {
     if (!all_init)
         return ESP_ERR_INVALID_STATE;
 
     ESP_RETURN_ON_ERROR(espnow_deinit(), TAG, "Failed to deinit espnow");
+
+    all_init = false;
+
     return ESP_OK;
 }
 
-esp_err_t bind(remote_ctrl_bind_config_t config,
+esp_err_t remote_ctrl_bind(remote_ctrl_bind_config_t config,
                bool bind)
 {
     ESP_RETURN_ON_ERROR(espnow_ctrl_responder_bind(config.wait_ms, config.rssi, NULL), TAG, "Failed to responder_bind");
@@ -71,24 +80,29 @@ esp_err_t bind(remote_ctrl_bind_config_t config,
     return ESP_OK;
 }
 
-esp_err_t set_data_cb(espnow_ctrl_data_cb_t cb)
+esp_err_t remote_ctrl_set_data_cb(espnow_ctrl_data_cb_t cb)
 {
     return espnow_ctrl_responder_data(cb);
 }
 
-esp_err_t send(espnow_attribute_t init_attr,
+esp_err_t remote_ctrl_send(espnow_attribute_t init_attr,
                espnow_attribute_t resp_attr,
                uint32_t val)
 {
     return espnow_ctrl_initiator_send(init_attr, resp_attr, val);
 }
 
-esp_err_t register_button_events(const button_handle_t handle,
-                                 espnow_attribute_t init_attr)
+esp_err_t remote_ctrl_register_button_events(const button_handle_t handle,
+                                 remote_ctrl_bind_config_t config)
 {
     ESP_PARAM_CHECK(handle);
 
-    ESP_RETURN_ON_ERROR(iot_button_register_cb(handle, BUTTON_DOUBLE_CLICK, button_double_click_cb, (void *)init_attr), TAG, "Failed to register double click cb");
+    ESP_LOGI(TAG, "Config{init_attr : %" PRIi16 ", rssi : %" PRIi8 ", wait_ms : %" PRIu16 ", resp_to_init_ms : %" PRIu16 "}, size of config: %u", config.init_attr, config.rssi, config.wait_ms, config.resp_to_init_ms, sizeof(config));
+
+    void *usr_data;
+    memcpy(&usr_data, &config, sizeof(void*));
+
+    ESP_RETURN_ON_ERROR(iot_button_register_cb(handle, BUTTON_DOUBLE_CLICK, button_double_click_cb, usr_data), TAG, "Failed to register double click cb");
 
     return ESP_OK;
 }
